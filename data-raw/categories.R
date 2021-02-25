d7ff83f4da03d72f602ef6b5e63020aeb79c2da5
josempozo <josmpozo@gmail.com>
2021-02-25 15:04:50 +0100

    Corrected one entrace of bota in dataValenzuelaLamas2008. Two measures missed a decimal comma.

--- data-raw/categories.R
+++ data-raw/categories.R
@@ -6,7 +6,13 @@ category <- list(
   capra = c("capra hircus", "capra aegagrus"),
   sus = c("sus domesticus", "sus scrofa"),
   caprine = c("ovis aries", "ovis orientalis",
-              "capra hircus", "capra aegagrus", "ovis/capra")
+              "capra hircus", "capra aegagrus", "ovis/capra"),
+  phal1 = c("first phalanx", "posterior first phalanx",
+                      "anterior first phalanx"),
+  phal2 = c("second phalanx", "posterior second phalanx",
+                      "anterior second phalanx"),
+  phal3 = c("third phalanx", "posterior third phalanx",
+                      "anterior third phalanx"),
 )
 
 usethis::use_data(category, overwrite = TRUE)
