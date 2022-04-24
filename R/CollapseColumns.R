6aef73ece93660294d996f6d2d471dc62952bf39
Jose M Pozo <72624361+josempozo@users.noreply.github.com>
2022-04-24 08:48:09 +0200

    ENH: Added SplitColumns as inverse to CollapseColumns

--- R/CollapseColumns.R
+++ R/CollapseColumns.R
@@ -4,3 +4,10 @@ CollapseColumns <- function(df, ..., sepMark = "--&&--")
 {
   apply(cbind(df, ...), 1, paste, collapse = sepMark)
 }
+
+SplitColumns <- function(x, colNames = NULL, sepMark = "--&&--")
+{
+  res <- as.data.frame(t(simplify2array(strsplit(x, sepMark, fixed = TRUE))))
+  colnames(res) <- colNames
+  return(res)
+}
