c3e76d66a2d3d7039d4d86c40f67714ae124feb2
Jose M Pozo <jose@MacBookPdeJose2.home>
2023-09-14 13:23:01 +0200

    ENH: FormatListOfNames with pre- and postMessage.

--- R/FormatListOfNames.R
+++ R/FormatListOfNames.R
@@ -1,12 +1,16 @@
 # Function to help format vector of names to be used in the documentation.
-FormatListOfNames <- function(names, formatMarks = c("\\emph{", "}"))
+FormatListOfNames <- function(names, formatMarks = c("\\emph{", "}"),
+                              preMessage = NULL, postMessage = NULL)
 {
   n <- length(names)
   if(n == 0) return("")
   formatedNames <- paste0(formatMarks[1], names, formatMarks[2])
-  if(n == 1) return(formatedNames)
-  formatedNames[n] <- paste0("and ", formatedNames[n])
+  preN <- min(length(preMessage), n)
+  postN <- min(length(postMessage), n)
+  if(n > 1) formatedNames[n] <- paste0("and ", formatedNames[n])
   comma <- ", "
   if(n == 2) comma <- " "
-  paste0(formatedNames, collapse = comma)
+  paste(c(preMessage[preN],
+          paste0(formatedNames, collapse = comma),
+          postMessage[postN]), collapse = " ")
 }
