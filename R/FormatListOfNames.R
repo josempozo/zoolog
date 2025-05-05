a0226704ab4aeeb386ba837855b4ecfe2afde900
josmpozo@gmail.com <josmpozo@gmail.com>
2025-05-05 10:09:03 +0200

    ENH: Add optional parameter conjunction to FormatListOfNames

--- R/FormatListOfNames.R
+++ R/FormatListOfNames.R
@@ -1,13 +1,14 @@
 # Function to help format vector of names to be used in the documentation.
 FormatListOfNames <- function(names, formatMarks = c("\\emph{", "}"),
-                              preMessage = NULL, postMessage = NULL)
+                              preMessage = NULL, postMessage = NULL,
+                              conjunction = "and")
 {
   n <- length(names)
   if(n == 0) return("")
   formatedNames <- paste0(formatMarks[1], names, formatMarks[2])
   preN <- min(length(preMessage), n)
   postN <- min(length(postMessage), n)
-  if(n > 1) formatedNames[n] <- paste0("and ", formatedNames[n])
+  if(n > 1) formatedNames[n] <- paste0(conjunction, " ", formatedNames[n])
   comma <- ", "
   if(n == 2) comma <- " "
   paste(c(preMessage[preN],
